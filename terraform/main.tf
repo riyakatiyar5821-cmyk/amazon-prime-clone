c19cce52a53cb1e3c9caaaf8dbeca4227a244550
Riya Katiyar <riyakatiyar5821@gmail.com>
2025-11-03 12:25:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "ec2_security_group" {
 
 resource "aws_instance" "Monitoring_server" {
 ami = "ami-0023921b4fcd5382b "  
-instance_type = "t2.medium"
+instance_type = "t3.medium"
 security_groups = [aws_security_group.ec2_security_group.name]
 key_name = "monitoring-key"
 tags = {

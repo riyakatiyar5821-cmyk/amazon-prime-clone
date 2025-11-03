8f620cfc2d75ce0c1b9910b7232604cd13fdc230
Riya Katiyar <riyakatiyar5821@gmail.com>
2025-11-03 11:55:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"   # uses credentials from `aws configure`
+  profile = "amazon-prime-clone"`
 }
 
 
